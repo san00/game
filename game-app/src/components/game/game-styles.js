9dfd28a34803dea89f1fcf0b7c650c17499d14a0
san00 <you@example.com>
2021-07-23 10:06:58 +0100

    Position buttons over background

--- game-app/src/components/game/game-styles.js
+++ game-app/src/components/game/game-styles.js
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 import { spacingScale } from "../../utils/spacing";
+import BG from "../../images/bg-triangle.svg";
 
 export const Wrapper = styled.section`
   display: flex;
   justify-content: center;
   margin: ${spacingScale.spacing_xl};
+  background-image: url(${BG});
+  background-repeat: no-repeat;
+  background-position: center center;
+  position: relative;
+  top: 8em;
+  z-index: -1;
 `;
 
 export const Button = styled.button`
@@ -20,4 +27,15 @@ export const Button = styled.button`
       ? "0 0 0 20px #eaab23"
       : "0 0 0 20px #ed5068"};
   filter: drop-shadow(10px 0px 10px rgba(0 0 0 / 30%));
+  position: relative;
+  left: ${(props) => {
+    if (props.left) return "4em";
+    if (props.leftL) return "-4em";
+    return "0";
+  }};
+  bottom: ${(props) => {
+    if (props.bottom) return "8em";
+    if (props.bottomL) return "-4em";
+    return "0";
+  }};
 `;
